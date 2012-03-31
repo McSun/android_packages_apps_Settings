20f212ed98e1a7365fbee3b3c7deb37a4a602fd7
Daniel Bergløv <webgeek.eu@gmail.com>
2012-03-31 14:41:42 +0200

    Lockscreen: Add toggle for lockscreen rotation (Settings part)

--- src/com/android/settings/cyanogenmod/DisplayRotation.java
+++ src/com/android/settings/cyanogenmod/DisplayRotation.java
@@ -38,12 +38,14 @@ public class DisplayRotation extends SettingsPreferenceFragment implements OnPre
     private static final String TAG = "DisplayRotation";
 
     private static final String KEY_ACCELEROMETER = "accelerometer";
+    private static final String KEY_ACCELEROMETER_LOCKSCREEN = "accelerometer_lockscreen";
     private static final String ROTATION_0_PREF = "display_rotation_0";
     private static final String ROTATION_90_PREF = "display_rotation_90";
     private static final String ROTATION_180_PREF = "display_rotation_180";
     private static final String ROTATION_270_PREF = "display_rotation_270";
 
     private CheckBoxPreference mAccelerometer;
+    private CheckBoxPreference mAccelerometerLS;
     private CheckBoxPreference mRotation0Pref;
     private CheckBoxPreference mRotation90Pref;
     private CheckBoxPreference mRotation180Pref;
@@ -71,6 +73,8 @@ public class DisplayRotation extends SettingsPreferenceFragment implements OnPre
 
         mAccelerometer = (CheckBoxPreference) findPreference(KEY_ACCELEROMETER);
         mAccelerometer.setPersistent(false);
+        mAccelerometerLS = (CheckBoxPreference) findPreference(KEY_ACCELEROMETER_LOCKSCREEN);
+        mAccelerometerLS.setPersistent(false);
         mRotation0Pref = (CheckBoxPreference) prefSet.findPreference(ROTATION_0_PREF);
         mRotation90Pref = (CheckBoxPreference) prefSet.findPreference(ROTATION_90_PREF);
         mRotation180Pref = (CheckBoxPreference) prefSet.findPreference(ROTATION_180_PREF);
@@ -131,7 +135,13 @@ public class DisplayRotation extends SettingsPreferenceFragment implements OnPre
             } catch (RemoteException exc) {
                 Log.w(TAG, "Unable to save auto-rotate setting");
             }
-        } else if (preference == mRotation0Pref ||
+        } else if (preference == mAccelerometerLS) {
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.ACCELEROMETER_LOCKSCREEN_ROTATION, 
+			mAccelerometerLS.isChecked() ? 1 : 0);
+            return true;
+
+	} else if (preference == mRotation0Pref ||
                 preference == mRotation90Pref ||
                 preference == mRotation180Pref ||
                 preference == mRotation270Pref) {
