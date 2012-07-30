8e6768c2fd2b456d5bce5074afb376e5b93433f0
Daniel Bergløv <webgeek.eu@gmail.com>
2012-07-30 22:52:36 +0200

    Processor: Add Sleep state to be used by third party
    Merged from:
    https://github.com/SpazeDog/android_packages_apps_Settings/commit/bf348af0ceb0ffdf8ac94a82eacee6d5f2d74678
    https://github.com/SpazeDog/android_packages_apps_Settings/commit/30f7a045cb9cbc6345c372a2d727a3ba7053f85c

--- src/com/android/settings/cyanogenmod/Processor.java
+++ src/com/android/settings/cyanogenmod/Processor.java
@@ -20,6 +20,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.preference.ListPreference;
+import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
 
@@ -47,6 +48,16 @@ public class Processor extends SettingsPreferenceFragment implements
     public static final String FREQ_MIN_FILE = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq";
     public static final String SOB_PREF = "pref_cpu_set_on_boot";
 
+    /* Controlls for the screen state scalling scripts sleep state */
+    public static final String GOV_PREF_SLEEP = "pref_cpu_gov_sleep";
+    public static final String FREQ_MIN_PREF_SLEEP = "pref_cpu_freq_min_sleep";
+    public static final String FREQ_MAX_PREF_SLEEP = "pref_cpu_freq_max_sleep";
+    public static final String GOV_FILE_SLEEP = "/data/property/cpu.sleep.governor";
+    public static final String FREQ_MAX_FILE_SLEEP = "/data/property/cpu.sleep.scaling.max";
+    public static final String FREQ_MIN_FILE_SLEEP = "/data/property/cpu.sleep.scaling.min";
+    public static final String ENABLE_FILE_SLEEP = "/data/property/cpu.sleep.switch";
+    public static final String ENABLE_PREF_SLEEP = "pref_cpu_enable_sleep";
+
     private static final String TAG = "CPUSettings";
 
     private String mGovernorFormat;
@@ -57,6 +68,12 @@ public class Processor extends SettingsPreferenceFragment implements
     private ListPreference mGovernorPref;
     private ListPreference mMinFrequencyPref;
     private ListPreference mMaxFrequencyPref;
+
+    /* Controlls for the screen state scalling scripts sleep state */
+    private ListPreference mGovernorSleepPref;
+    private ListPreference mMinFrequencySleepPref;
+    private ListPreference mMaxFrequencySleepPref;
+    private CheckBoxPreference mEnableSleepPref;
 
     private class CurCPUThread extends Thread {
         private boolean mInterrupt = false;
@@ -107,14 +124,26 @@ public class Processor extends SettingsPreferenceFragment implements
         PreferenceScreen prefScreen = getPreferenceScreen();
 
         mGovernorPref = (ListPreference) prefScreen.findPreference(GOV_PREF);
+	mGovernorSleepPref = (ListPreference) prefScreen.findPreference(GOV_PREF_SLEEP);
         mCurFrequencyPref = (Preference) prefScreen.findPreference(FREQ_CUR_PREF);
         mMinFrequencyPref = (ListPreference) prefScreen.findPreference(FREQ_MIN_PREF);
         mMaxFrequencyPref = (ListPreference) prefScreen.findPreference(FREQ_MAX_PREF);
+	mMinFrequencySleepPref = (ListPreference) prefScreen.findPreference(FREQ_MIN_PREF_SLEEP);
+	mMaxFrequencySleepPref = (ListPreference) prefScreen.findPreference(FREQ_MAX_PREF_SLEEP);
+	mEnableSleepPref = (CheckBoxPreference) prefScreen.findPreference(ENABLE_PREF_SLEEP);
+
+	if (Utils.fileExists(ENABLE_FILE_SLEEP) == false || (temp = Utils.fileReadOneLine(ENABLE_FILE_SLEEP)) == null) {
+		mEnableSleepPref.setEnabled(false);
+
+	} else {
+		mEnableSleepPref.setChecked("1".equals(temp));
+	}
 
         /* Governor
         Some systems might not use governors */
         if (!Utils.fileExists(GOV_LIST_FILE) || !Utils.fileExists(GOV_FILE) || (temp = Utils.fileReadOneLine(GOV_FILE)) == null || (availableGovernorsLine = Utils.fileReadOneLine(GOV_LIST_FILE)) == null) {
-            prefScreen.removePreference(mGovernorPref);
+            mGovernorPref.setEnabled(false);
+	    mGovernorSleepPref.setEnabled(false);
 
         } else {
             availableGovernors = availableGovernorsLine.split(" ");
@@ -124,12 +153,25 @@ public class Processor extends SettingsPreferenceFragment implements
             mGovernorPref.setValue(temp);
             mGovernorPref.setSummary(String.format(mGovernorFormat, temp));
             mGovernorPref.setOnPreferenceChangeListener(this);
+
+	    if (Utils.fileExists(GOV_FILE_SLEEP) == false || (temp = Utils.fileReadOneLine(GOV_FILE_SLEEP)) == null) {
+		    mGovernorSleepPref.setEnabled(false);
+
+	    } else {
+		    mGovernorSleepPref.setEntryValues(availableGovernors);
+		    mGovernorSleepPref.setEntries(availableGovernors);
+		    mGovernorSleepPref.setValue(temp);
+		    mGovernorSleepPref.setSummary(String.format(mGovernorFormat, temp));
+		    mGovernorSleepPref.setOnPreferenceChangeListener(this);
+	    }
         }
 
         // Disable the min/max list if we dont have a list file
         if (!Utils.fileExists(FREQ_LIST_FILE) || (availableFrequenciesLine = Utils.fileReadOneLine(FREQ_LIST_FILE)) == null) {
             mMinFrequencyPref.setEnabled(false);
             mMaxFrequencyPref.setEnabled(false);
+            mMinFrequencySleepPref.setEnabled(false);
+            mMaxFrequencySleepPref.setEnabled(false);
 
         } else {
             availableFrequencies = availableFrequenciesLine.split(" ");
@@ -142,6 +184,7 @@ public class Processor extends SettingsPreferenceFragment implements
             // Min frequency
             if (!Utils.fileExists(FREQ_MIN_FILE) || (temp = Utils.fileReadOneLine(FREQ_MIN_FILE)) == null) {
                 mMinFrequencyPref.setEnabled(false);
+		mMinFrequencySleepPref.setEnabled(false);
 
             } else {
                 mMinFrequencyPref.setEntryValues(availableFrequencies);
@@ -149,11 +192,23 @@ public class Processor extends SettingsPreferenceFragment implements
                 mMinFrequencyPref.setValue(temp);
                 mMinFrequencyPref.setSummary(String.format(mMinFrequencyFormat, toMHz(temp)));
                 mMinFrequencyPref.setOnPreferenceChangeListener(this);
+
+		if (Utils.fileExists(FREQ_MIN_FILE_SLEEP) == false || (temp = Utils.fileReadOneLine(FREQ_MIN_FILE_SLEEP)) == null) {
+			mMinFrequencySleepPref.setEnabled(false);
+
+		} else {
+			mMinFrequencySleepPref.setEntryValues(availableFrequencies);
+			mMinFrequencySleepPref.setEntries(frequencies);
+			mMinFrequencySleepPref.setValue(temp);
+			mMinFrequencySleepPref.setSummary(String.format(mMinFrequencyFormat, toMHz(temp)));
+			mMinFrequencySleepPref.setOnPreferenceChangeListener(this);
+		}
             }
 
             // Max frequency
             if (!Utils.fileExists(FREQ_MAX_FILE) || (temp = Utils.fileReadOneLine(FREQ_MAX_FILE)) == null) {
                 mMaxFrequencyPref.setEnabled(false);
+		mMaxFrequencySleepPref.setEnabled(false);
 
             } else {
                 mMaxFrequencyPref.setEntryValues(availableFrequencies);
@@ -161,6 +216,17 @@ public class Processor extends SettingsPreferenceFragment implements
                 mMaxFrequencyPref.setValue(temp);
                 mMaxFrequencyPref.setSummary(String.format(mMaxFrequencyFormat, toMHz(temp)));
                 mMaxFrequencyPref.setOnPreferenceChangeListener(this);
+
+		if (Utils.fileExists(FREQ_MAX_FILE_SLEEP) == false || (temp = Utils.fileReadOneLine(FREQ_MAX_FILE_SLEEP)) == null) {
+			mMaxFrequencySleepPref.setEnabled(false);
+
+		} else {
+			mMaxFrequencySleepPref.setEntryValues(availableFrequencies);
+			mMaxFrequencySleepPref.setEntries(frequencies);
+			mMaxFrequencySleepPref.setValue(temp);
+			mMaxFrequencySleepPref.setSummary(String.format(mMaxFrequencyFormat, toMHz(temp)));
+			mMaxFrequencySleepPref.setOnPreferenceChangeListener(this);
+		}
             }
         }
 
@@ -195,9 +261,23 @@ public class Processor extends SettingsPreferenceFragment implements
             mMaxFrequencyPref.setSummary(String.format(mMaxFrequencyFormat, toMHz(temp)));
         }
 
+	if (Utils.fileExists(FREQ_MIN_FILE_SLEEP) && (temp = Utils.fileReadOneLine(FREQ_MIN_FILE_SLEEP)) != null) {
+		mMinFrequencySleepPref.setValue(temp);
+		mMinFrequencySleepPref.setSummary(String.format(mMinFrequencyFormat, toMHz(temp)));
+	}
+
+	if (Utils.fileExists(FREQ_MAX_FILE_SLEEP) && (temp = Utils.fileReadOneLine(FREQ_MAX_FILE_SLEEP)) != null) {
+        	mMaxFrequencySleepPref.setValue(temp);
+        	mMaxFrequencySleepPref.setSummary(String.format(mMaxFrequencyFormat, toMHz(temp)));
+	}
+
         if (Utils.fileExists(GOV_FILE) && (temp = Utils.fileReadOneLine(GOV_FILE)) != null) {
             mGovernorPref.setSummary(String.format(mGovernorFormat, temp));
         }
+
+	if (Utils.fileExists(GOV_FILE_SLEEP) && (temp = Utils.fileReadOneLine(GOV_FILE_SLEEP)) != null) {
+        	mGovernorSleepPref.setSummary(String.format(mGovernorFormat, temp));
+	}
     }
 
     @Override
@@ -210,26 +290,49 @@ public class Processor extends SettingsPreferenceFragment implements
         }
     }
 
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+	if (preference == mEnableSleepPref) {
+		return Utils.fileWriteOneLine(ENABLE_FILE_SLEEP, mEnableSleepPref.isChecked() ? "1" : "0") ? true : false;
+	}
+
+	return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         String fname = "";
 
         if (newValue != null) {
             if (preference == mGovernorPref) {
                 fname = GOV_FILE;
+            } else if (preference == mGovernorSleepPref) {
+                fname = GOV_FILE_SLEEP;
             } else if (preference == mMinFrequencyPref) {
                 fname = FREQ_MIN_FILE;
             } else if (preference == mMaxFrequencyPref) {
                 fname = FREQ_MAX_FILE;
+            } else if (preference == mMinFrequencySleepPref) {
+                fname = FREQ_MIN_FILE_SLEEP;
+            } else if (preference == mMaxFrequencySleepPref) {
+                fname = FREQ_MAX_FILE_SLEEP;
             }
 
             if (Utils.fileWriteOneLine(fname, (String) newValue)) {
                 if (preference == mGovernorPref) {
                     mGovernorPref.setSummary(String.format(mGovernorFormat, (String) newValue));
+                } else if (preference == mGovernorSleepPref) {
+                    mGovernorSleepPref.setSummary(String.format(mGovernorFormat, (String) newValue));
                 } else if (preference == mMinFrequencyPref) {
                     mMinFrequencyPref.setSummary(String.format(mMinFrequencyFormat,
                             toMHz((String) newValue)));
                 } else if (preference == mMaxFrequencyPref) {
                     mMaxFrequencyPref.setSummary(String.format(mMaxFrequencyFormat,
+                            toMHz((String) newValue)));
+                } else if (preference == mMinFrequencySleepPref) {
+                    mMinFrequencySleepPref.setSummary(String.format(mMinFrequencyFormat,
+                            toMHz((String) newValue)));
+                } else if (preference == mMaxFrequencySleepPref) {
+                    mMaxFrequencySleepPref.setSummary(String.format(mMaxFrequencyFormat,
                             toMHz((String) newValue)));
                 }
                 return true;
